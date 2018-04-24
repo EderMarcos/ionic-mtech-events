01edcbed2404f1e586aecfd1d2de06c7d083586a
(Developer) Eder Marcos <eder.marcos@mtech-systems.com>
2018-04-24 11:30:52 -0500

    Respaldo

--- src/components/mt-card-component/mt-card-component.ts
+++ src/components/mt-card-component/mt-card-component.ts
@@ -14,7 +14,7 @@ export class MtCardComponent {
   @Input() imgBackground: string = 'https://picsum.photos/150/150';
   @Input() title: string = 'CTM 2015 - Totally Enormous Extinct Dinosaurus, Greco-Roman';
   @Input() location: string = 'Berghain / Panorama Bar';
-  @Input() date: string = 'Today 23:00 - 11:00';
+  @Input() date: string = 'Today May 22, 2018 9:15 - 10:15 AM';
 
   onCardClick() {
     this.navCtrl.push(MtMapsPage, { event });
