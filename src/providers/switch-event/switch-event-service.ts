3976e14c9eed988c1bde178e4d5aab9dc140e481
(Developer) Eder Marcos <eder.marcos@mtech-systems.com>
2018-04-25 14:52:31 -0500

    Bg mode test 11

--- src/providers/switch-event/switch-event-service.ts
+++ src/providers/switch-event/switch-event-service.ts
@@ -40,26 +40,7 @@ export class SwitchEventService {
       if (this.platform.is('cordova')) {
         this.backgroundMode.on('activate').subscribe(() => {
           this.backgroundMode.disableWebViewOptimizations();
-          for (let i = 0; i < events.length; i++) {
-            if (now > events[events.length - 1].endTime) {
-              // At the end Event
-              observer.next(this.onNullEvent);
-              return observer.complete();
-            }
-            if (now < events[0].date) {
-              // At the start Event
-              observer.next(this.onNullEvent);
-            }
-            if (now > events[i].endTime && events[i].available) {
-              events[i].available = false;
-              this.dataService.updateEntity({ collection: 'events', key: events[i].id }, events[i]);
-            } else {
-              this.getEventByDate(events[i], lastEvent)
-                .then((event: EventInterface) => observer.next(event))
-            }
-          }
         });
-        return;
       }
       for (let i = 0; i < events.length; i++) {
         if (now > events[events.length - 1].endTime) {
